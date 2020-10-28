a28be99c5f078a843e45ecfcb89a2d0a867c4d52
Aulang <aulang@qq.com>
2020-10-28 22:38:34 +0800

    完善

--- UI/js/admin/index.js
+++ UI/js/admin/index.js
@@ -1,6 +1,40 @@
 let index = new Vue({
     el: '#index',
     data: {
-        isWebsite: true
+        isWebsite: true,
+        config: {
+            title: '',
+            desc: '',
+            keywords: '',
+            author: '',
+            website: '',
+            email: '',
+            github: '',
+            wechat: '',
+            wechatQRCode: '',
+            avatar: '',
+            since: '',
+            links: [{
+                title: '',
+                url: '',
+                desc: ''
+            }]
+        },
+        menus: [{
+            id: '',
+            name: '',
+            url: '',
+            desc: '',
+            order: ''
+        }],
+        articles: [{
+            id: '',
+            title: '',
+            subTitle: '',
+            categoryName: '',
+            creationDate: '',
+            renew: '',
+            commentsCount: 0
+        }]
     }
 });
