53db099ca5b15c8f2a104c7c22ca120117ef857a
Aulang <aulang@qq.com>
2020-10-29 21:50:01 +0800

    完善

--- UI/js/admin/index.js
+++ UI/js/admin/index.js
@@ -1,3 +1,5 @@
+import {apiUrl} from "../public/base.js";
+
 let index = new Vue({
     el: '#index',
     data: {
@@ -36,5 +38,81 @@ let index = new Vue({
             renew: '',
             commentsCount: 0
         }]
+    },
+    methods: {
+        addMenu: function () {
+            alert('添加菜单！');
+        },
+        addLink: function () {
+            alert('添加友情链接！');
+        },
+        newArticle: function () {
+            alert('新文章！');
+        }
     }
 });
+
+function getConfig() {
+    axios.get(apiUrl + 'config')
+        .then(function (response) {
+            let code = response.data.code;
+            if (code !== 0) {
+                alert(response.data.msg);
+                return;
+            }
+
+            if (!response.data.data) {
+                return;
+            }
+
+            index.config = response.data.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function getMenus() {
+    axios.get(apiUrl + 'menus')
+        .then(function (response) {
+            let code = response.data.code;
+            if (code !== 0) {
+                alert(response.data.msg);
+                return;
+            }
+
+            if (!response.data.data) {
+                return;
+            }
+
+            index.menus = response.data.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function getArticles(page, size, keyword) {
+    let url = apiUrl + `articles/page?page=${page}&size=${size}&keyword=${keyword}`;
+    axios.get(url)
+        .then(function (response) {
+            let code = response.data.code;
+            if (code !== 0) {
+                alert(response.data.msg);
+                return;
+            }
+
+            if (!response.data.data.datas) {
+                return;
+            }
+
+            index.articles = response.data.data.datas;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+getMenus();
+getConfig();
+getArticles(1, 10, '');
